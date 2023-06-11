3d12dcdc4958fe9ad1923e5fcf410f78c2ca64bb
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-06-10 20:29:45 -0400

    first commit for quiz app

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="nav--logo_text">Quiz App Built with React</h3>
+            <h4 className="nav--title">Brian O'Neil</h4>
+        </nav>
+    )
+}
