3b53b3809d8e562db818d1d26d827e94c1806adb
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-06-17 17:14:59 -0400

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import questions from './questionsData';
 import Navbar from './components/Navbar';
 import QuestionCard from './components/QuestionCard';
